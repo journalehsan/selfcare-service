8902e65509732391f5c187b6dd21369e4882650f
journalehsan <ehsan.tor@mtnirancell.ir>
2025-08-09 00:49:38 +0330

    Improve and bugfix for new version

--- SelfcareService/obj/Debug/net9.0/SelfcareService.AssemblyInfo.cs
+++ SelfcareService/obj/Debug/net9.0/SelfcareService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelfcareService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051eb71645670f9920d5448c88228ab1380baf9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a01c070bfe82ebe9456402c2cd059f11ab01d0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelfcareService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelfcareService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
